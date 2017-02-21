db61f28e52ac35127582669d5cf9e95e8eb5684c
Carlos Alberto Villamizar Morantes <calvimor@gmail.com>
2017-02-21 09:04:59 -0500

    Communicating with child component using @Input Passing data from component to a child

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'events-list',
     templateUrl: 'app/events/events-list.component.html'
-
 })
 export class EventsListComponent {
     event = {
